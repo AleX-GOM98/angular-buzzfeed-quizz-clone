2ac06cd90d1ceeba29b96059f7ed6286b3180464
Alex <alex.gmaia98@gmail.com>
2024-04-23 22:33:36 -0300

    Projeo completo

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-projeto-buzzfeed'),
+      dir: require('path').join(__dirname, './coverage/projeto-buzzfeed'),
       subdir: '.',
       reporters: [
         { type: 'html' },
